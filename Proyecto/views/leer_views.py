542c4292489dc824f41d686d920b7982b3d1d42e
Samero0 <148150539+Samero0@users.noreply.github.com>
2024-12-13 09:19:48 +0000

    leer_views actualizado y estilo en main_view

--- Proyecto/views/leer_views.py
+++ Proyecto/views/leer_views.py
@@ -28,39 +28,243 @@ class LeerViews:
         self.page.window_height = 600
 
         self.page.update()
-        # Crear columnas de la tabla
-        columns = [
-            ft.DataColumn(ft.Text("Nombre")),
-            ft.DataColumn(ft.Text("Especialidad")),
-            ft.DataColumn(ft.Text("Teléfono")),
-            ft.DataColumn(ft.Text("E-mail"))
+
+        paciente_fields = ft.Column(visible=False)
+        medico_fields = ft.Column(visible=False)
+        cita_fields = ft.Column(visible=False)
+        botones_fields = ft.Column(visible=True)
+        # boton_volver = ft.Column(visible=False)
+
+        def mostrar_campos(tipo):
+            '''Metodo para mostrar campos dinámicamente según el botón presionado'''
+            self.collection = tipo
+
+            botones_fields.visible = False
+            paciente_fields.visible = tipo == "Paciente"
+            medico_fields.visible = tipo == "Médico"
+            cita_fields.visible = tipo == "Cita"
+            # boton_volver.visible = tipo == "Volver"
+            self.page.update()
+
+        def volver_a_menu_principal(e):
+            '''Método para mostrar de nuevo los botones principales y ocultar los campos'''
+            botones_fields.visible = True
+            paciente_fields.visible = False
+            medico_fields.visible = False
+            cita_fields.visible = False
+            self.page.update()
+
+        def mostrar_busqueda(e):
+            pass
+
+        medico_fields.controls = [
+            ft.Container(
+                content=ft.DataTable(
+                    columns=[
+                        ft.DataColumn(ft.Text("Nombre")),
+                        ft.DataColumn(ft.Text("Especialidad")),
+                        ft.DataColumn(ft.Text("Teléfono")),
+                        ft.DataColumn(ft.Text("E-mail")),
+                    ],
+                    rows=[
+                        ft.DataRow(cells=[
+                            ft.DataCell(ft.Text("Juan Pérez")),
+                            ft.DataCell(ft.Text("Oftalmología")),
+                            ft.DataCell(ft.Text("666666666")),
+                            ft.DataCell(ft.Text("juan@ejemplo.com")),
+                        ]),
+                        ft.DataRow(cells=[
+                            ft.DataCell(ft.Text("Ana Gómez")),
+                            ft.DataCell(ft.Text("Pediatría")),
+                            ft.DataCell(ft.Text("777777777")),
+                            ft.DataCell(ft.Text("ana@ejemplo.com")),
+                        ]),
+                        ft.DataRow(cells=[
+                            ft.DataCell(ft.Text("Carlos López")),
+                            ft.DataCell(ft.Text("Cardiología")),
+                            ft.DataCell(ft.Text("888888888")),
+                            ft.DataCell(ft.Text("carlos@ejemplo.com")),
+                        ]),
+                    ]              
+                ),
+                alignment=ft.alignment.center,  # Centra la tabla
+            ),
+        ft.Container(
+            content=ft.Column(
+                controls=[
+                    ft.ElevatedButton(
+                        text="Volver",
+                        width=300,
+                        height=50,
+                        on_click=volver_a_menu_principal
+                    ),
+                    ft.ElevatedButton(
+                        text="Buscar",
+                        width=300,
+                        height=50,
+                        on_click=volver_a_menu_principal
+                    )
+                ],
+                alignment=ft.MainAxisAlignment.CENTER  # Centra los botones verticalmente
+            ),
+            alignment=ft.alignment.center  # Centra toda la columna en el contenedor
+        )     
+    ]
+
+        paciente_fields.controls = [
+            ft.Container(
+                content=ft.DataTable(
+                    columns=[
+                        ft.DataColumn(ft.Text("Nombre")),
+                        ft.DataColumn(ft.Text("Especialidad")),
+                        ft.DataColumn(ft.Text("Teléfono")),
+                        ft.DataColumn(ft.Text("E-mail")),
+                    ],
+                    rows=[
+                        ft.DataRow(cells=[
+                            ft.DataCell(ft.Text("Juan Pérez")),
+                            ft.DataCell(ft.Text("Oftalmología")),
+                            ft.DataCell(ft.Text("666666666")),
+                            ft.DataCell(ft.Text("juan@ejemplo.com")),
+                        ]),
+                        ft.DataRow(cells=[
+                            ft.DataCell(ft.Text("Ana Gómez")),
+                            ft.DataCell(ft.Text("Pediatría")),
+                            ft.DataCell(ft.Text("777777777")),
+                            ft.DataCell(ft.Text("ana@ejemplo.com")),
+                        ]),
+                        ft.DataRow(cells=[
+                            ft.DataCell(ft.Text("Carlos López")),
+                            ft.DataCell(ft.Text("Cardiología")),
+                            ft.DataCell(ft.Text("888888888")),
+                            ft.DataCell(ft.Text("carlos@ejemplo.com")),
+                        ]),
+                    ]              
+                ),
+                alignment=ft.alignment.center,  # Centra la tabla
+            ),
+        ft.Container(
+            content=ft.Column(
+                controls=[
+                    ft.ElevatedButton(
+                        text="Volver",
+                        width=300,
+                        height=50,
+                        on_click=volver_a_menu_principal
+                    ),
+                    ft.ElevatedButton(
+                        text="Buscar",
+                        width=300,
+                        height=50,
+                        on_click=volver_a_menu_principal
+                    )
+                ],
+                alignment=ft.MainAxisAlignment.CENTER  # Centra los botones verticalmente
+            ),
+            alignment=ft.alignment.center  # Centra toda la columna en el contenedor
+        )     
+    ]
+
+        cita_fields.controls = [
+            ft.Container(
+                content=ft.DataTable(
+                    columns=[
+                        ft.DataColumn(ft.Text("Nombre")),
+                        ft.DataColumn(ft.Text("Especialidad")),
+                        ft.DataColumn(ft.Text("Teléfono")),
+                        ft.DataColumn(ft.Text("E-mail")),
+                    ],
+                    rows=[
+                        ft.DataRow(cells=[
+                            ft.DataCell(ft.Text("Juan Pérez")),
+                            ft.DataCell(ft.Text("Oftalmología")),
+                            ft.DataCell(ft.Text("666666666")),
+                            ft.DataCell(ft.Text("juan@ejemplo.com")),
+                        ]),
+                        ft.DataRow(cells=[
+                            ft.DataCell(ft.Text("Ana Gómez")),
+                            ft.DataCell(ft.Text("Pediatría")),
+                            ft.DataCell(ft.Text("777777777")),
+                            ft.DataCell(ft.Text("ana@ejemplo.com")),
+                        ]),
+                        ft.DataRow(cells=[
+                            ft.DataCell(ft.Text("Carlos López")),
+                            ft.DataCell(ft.Text("Cardiología")),
+                            ft.DataCell(ft.Text("888888888")),
+                            ft.DataCell(ft.Text("carlos@ejemplo.com")),
+                        ]),
+                    ]              
+                ),
+                alignment=ft.alignment.center,  # Centra la tabla
+            ),
+        ft.Container(
+            content=ft.Column(
+                controls=[
+                    ft.ElevatedButton(
+                        text="Volver",
+                        width=300,
+                        height=50,
+                        on_click=volver_a_menu_principal
+                    ),
+                    ft.ElevatedButton(
+                        text="Buscar",
+                        width=300,
+                        height=50,
+                        on_click=volver_a_menu_principal
+                    )
+                ],
+                alignment=ft.MainAxisAlignment.CENTER  # Centra los botones verticalmente
+            ),
+            alignment=ft.alignment.center  # Centra toda la columna en el contenedor
+        )     
+    ]
+
+        botones_fields.controls = [
+            ft.ElevatedButton(
+                text="Paciente",
+                width=300,
+                height=50,
+                on_click=lambda e: mostrar_campos("Paciente"),
+            ),
+            ft.ElevatedButton(
+                text="Médico",
+                width=300,
+                height=50,
+                on_click=lambda e: mostrar_campos("Médico"),
+            ),
+            ft.ElevatedButton(
+                text="Cita",
+                width=300,
+                height=50,
+                on_click=lambda e: mostrar_campos("Cita"),
+            ),
+            ft.ElevatedButton(
+                text="Volver",
+                width=300,
+                height=50,
+                on_click=lambda e: ir_a_main("Volver"),
+            ),
         ]
-        
-        # Crear filas de la tabla
-        rows = [
-            ft.DataRow(cells=[
-                ft.DataCell(ft.Text("Juan Pérez")),
-                ft.DataCell(ft.Text("Oftalmología")),
-                ft.DataCell(ft.Text("666666666")),
-                ft.DataCell(ft.Text("juan@ejemplo.com"))
-            ]),
-            ft.DataRow(cells=[
-                ft.DataCell(ft.Text("Ana Gómez")),
-                ft.DataCell(ft.Text("32")),
-                ft.DataCell(ft.Text("Lima")),
-                ft.DataCell(ft.Text("Lima"))
-            ]),
-            ft.DataRow(cells=[
-                ft.DataCell(ft.Text("Carlos López")),
-                ft.DataCell(ft.Text("24")),
-                ft.DataCell(ft.Text("Ciudad de México")),
-                ft.DataCell(ft.Text("Lima"))
-            ]),
-        ]
-        
-        # Crear DataTable
-        tabla = ft.DataTable(columns=columns, rows=rows)
-        
-        self.page.add(tabla)
-        # Agregar la tabla a la páginaº
+
+        def ir_a_main(e):
+            from views import MainView
+            # Limpiar la página actual
+            self.page.clean()
+
+            # Crear una instancia de MainView y construirla
+            main_view = MainView(self.page)
+            main_view.build()
+
+        # Agregar los botones y los contenedores dinámicos a la página
+        self.page.add(
+            ft.Container(
+                content=ft.Column(
+                    controls=[botones_fields, paciente_fields, medico_fields, cita_fields],
+                    alignment=ft.MainAxisAlignment.CENTER,
+                    spacing=20,
+                ),
+                padding=20,
+            )
+        )
+
         
--- Proyecto/views/main_view.py
+++ Proyecto/views/main_view.py
@@ -1,6 +1,7 @@
 import flet as ft
+
 '''
-fichero que contiene la clase de la pagina principal
+Fichero que contiene la clase de la página principal
 '''
 
 class MainView:
@@ -8,68 +9,113 @@ class MainView:
         self.page = page
 
     def build(self):
-        '''Metodo que construlle la pagina principal'''
+        '''Método que construye la página principal'''
 
-        self.page.title = "Pagina principal"
+        self.page.title = "Página Principal"
 
+        # Centrar los elementos de la página
         self.page.vertical_alignment = ft.MainAxisAlignment.CENTER
         self.page.horizontal_alignment = ft.CrossAxisAlignment.CENTER
 
-    
-        #tamaño en px
+        # Tamaño de la ventana
         self.page.window_width = 400
-        self.page.window_height = 500
+        self.page.window_height = 550
 
-        #para que no se pueda redimensionar
+        # Evitar que se pueda redimensionar la ventana
         self.page.window_resizable = False
         self.page.window_full_screen = False
 
-    #crea un contenedor con los botones alineados en el centro de la pagina
+        self.page.bgcolor = ft.colors.WHITE
+
+        # Estilo de los botones (color, bordes redondeados y tamaño)
+        button_style = ft.ButtonStyle(
+            bgcolor="#FFA07A",
+            color="black",
+            overlay_color="#D35400",  # Color al hacer clic
+            shape=ft.RoundedRectangleBorder(radius=10)  # Bordes redondeados
+        )
+
+        # Botones principales (Aplicación de estilo general)
         botones = ft.Container(
             content=ft.Column(
                 controls=[
-                    ft.ElevatedButton(text="Leer Datos", width=300, height=50, on_click=self.ir_a_leer),
-                    ft.ElevatedButton(text="Insertar Datos", width=300, height=50, on_click=self.ir_a_insersiones),
-                    ft.ElevatedButton(text="Actualizar Datos", width=300, height=50),
-                    ft.ElevatedButton(text="Eliminar Datos", width=300, height=50),
-                    ft.ElevatedButton(text="Generar Datos", width=300, height=50),
-                    ft.ElevatedButton(text="Salir", width=300, height=50)
+                    ft.ElevatedButton(
+                        text="Leer Datos",
+                        width=300,
+                        height=50,
+                        style=button_style,
+                        on_click=self.ir_a_leer,
+                    ),
+                    ft.ElevatedButton(
+                        text="Insertar Datos",
+                        width=300,
+                        height=50,
+                        style=button_style,
+                        on_click=self.ir_a_insersiones,
+                    ),
+                    ft.ElevatedButton(
+                        text="Actualizar Datos",
+                        width=300,
+                        height=50,
+                        style=button_style,
+                    ),
+                    ft.ElevatedButton(
+                        text="Eliminar Datos",
+                        width=300,
+                        height=50,
+                        style=button_style,
+                    ),
+                    ft.ElevatedButton(
+                        text="Generar Datos",
+                        width=300,
+                        height=50,
+                        style=button_style,
+                    ),
+                    ft.ElevatedButton(
+                        text="Salir",
+                        width=300,
+                        height=50,
+                        style=button_style,
+                        on_click=self.cerrar,
+                    ),
                 ],
                 alignment=ft.MainAxisAlignment.CENTER,
-                spacing=15,  #espacio entre los botones
+                spacing=15,  # Espacio entre los botones
             ),
             padding=10,
         )
 
-    
+        # Agregar los botones y título a la parte superior de la página
+        self.page.add(
+            ft.Container(
+                content=ft.Text(
+                    "Menú Principal",
+                    size=28,
+                    weight="bold",
+                    color="Black"
+                ),
+                alignment=ft.alignment.center,
+                margin=ft.Margin(0, 20, 0, 20)  # Margen superior e inferior
+            )
+        )
 
-        # Agregar los botones a la parte inferior de la página
-        self.page.add(ft.Container(height=20))  # Espacio vacío arriba
-        self.page.add(botones)  # Botones en la parte inferior
+        self.page.add(botones)
         self.page.update()
 
+    def cerrar(self, e):
+        '''Función que se llama al presionar el botón "Salir" y cierra la ventana.'''
+        self.page.window_close()
+
     def ir_a_insersiones(self, e):
-        '''Función que se llama al presionar el botón "Insertar Datos"
-        y abre la pagina "InsersionesVew"
-        '''
-
+        '''Función que se llama al presionar el botón "Insertar Datos" y abre la página "InsercionesView".'''
         from views import InsercionesView
-        # Limpiar la página actual
-        self.page.clean()
-
-        # Crear una instancia de InsersionesView y construirla
+        self.page.clean()  # Limpiar la página actual
         insersiones_view = InsercionesView(self.page)
         insersiones_view.build()
 
     def ir_a_leer(self, e):
-        '''Función que se llama al presionar el botón "Leer Datos"
-        y abre la pagina "LeerView"
-        '''
-
+        '''Función que se llama al presionar el botón "Leer Datos" y abre la página "LeerViews".'''
         from views import LeerViews
-        # Limpiar la página actual
-        self.page.clean()
-
-        # Crear una instancia de LeerView y construirla
-        leer_view = LeerViews(self.page)
-        leer_view.build()
+        self.page.clean()  # Limpiar la página actual
+        submenu_view = LeerViews(self.page)
+        submenu_view.build()

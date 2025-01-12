d6439a276df0433eb96ac6fe43b624295fbeb6eb
yoadria <tuadria@gmail.com>
2025-01-12 17:55:41 +0000

    pequeña midificacion en utils

--- Proyecto/utils/__init__.py
+++ Proyecto/utils/__init__.py
@@ -1,7 +1,7 @@
-from .labels import LABELS_MAP, COLUMNAS_MAP, ENCABEZADOS_MAPA
+from .labels import LABELS_MAP
 from .style import  button_style, estilo_encabezado, estilo_celda, estilo_tabla
 from .validators import validacion_id, validar_email, validar_telefono, validar_dni, validar_edad
 
-__all__ = ["ENCABEZADOS_MAPA","COLUMNAS_MAP","LABELS_MAP", "button_style", "estilo_encabezado", 
+__all__ = ["LABELS_MAP", "button_style", "estilo_encabezado", 
            "estilo_celda", "estilo_tabla", "validacion_id", "validar_email", "validar_telefono", 'validar_dni',
            'validar_edad']
